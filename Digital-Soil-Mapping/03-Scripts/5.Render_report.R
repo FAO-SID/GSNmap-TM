0b37da6a43a9faf55db4b7a3f848e5e59280b695
Isabel Luotto <72745296+Isa-Luotto@users.noreply.github.com>
2022-12-02 10:34:32 +0100

    fixed issue with report

--- Digital-Soil-Mapping/03-Scripts/5.Render_report.R
+++ Digital-Soil-Mapping/03-Scripts/5.Render_report.R
@@ -1,8 +1,14 @@
 # 1 - Open the file C:/GIT/GSNmap-TM/Digital-Soil-Mapping/Report_k.Rmd
 # 2 - adjust the content
 # 3 - Run the following code
-path = 'C:/GIT/GSNmap-TM/Digital-Soil-Mapping/Report_k.Rmd'
-output_format = 'pdf_document'
-output_file = paste0("Report_", soilatt,".pdf")
+wd <-'C:/Users/luottoi/Documents/GitHub/GSNmap-TM/Digital-Soil-Mapping/National Report' 
+setwd(wd)
+
+ISO <- 'AOI'
+
+
+path = 'National GSNmap Report.Rmd'
+output_format = 'word_document'
+output_file = paste0("Report_GSNmap_",ISO,".docx")
 
 rmarkdown::render(path, output_format, output_file)
