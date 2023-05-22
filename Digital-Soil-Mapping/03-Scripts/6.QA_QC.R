03b6d3bafb770eb2a70f4cfc6dd7454fc40a6942
Isabel Luotto <72745296+Isa-Luotto@users.noreply.github.com>
2023-05-22 15:22:28 +0200

    added property overview table

--- Digital-Soil-Mapping/03-Scripts/6.QA_QC.R
+++ Digital-Soil-Mapping/03-Scripts/6.QA_QC.R
@@ -38,8 +38,12 @@ library(terra)
 library(readxl)
 
 # Load reference values
-dt <- read_xlsx("../tables/wosis_dist.xlsx")
+dt <- read_xlsx("C:/Users/luottoi/Documents/GitHub/GSNmap-TM/tables/wosis_dist.xlsx")
 dt <- dt[!(dt$`Soil property` %in%c( "P Bray I","P Olsen" )),]
+
+# In case old naming system was used
+dt$old_prop_ids <- c('Ntot', 'Pav', 'CEC','pH', 'Clay', 'Silt', 'Sand', 'SOC', 'BD', 'Kav')
+
 
 ## Set potential ranges for Available K in ppm
 
@@ -53,18 +57,23 @@ dt[dt$property_id=='k_0_30','Max'] <- 150
 
 ## Check if all layers were correctly generated (including uncertainty layers)
 # and if the correct ISO code and soil property ids were included in the files names
-files <- list.files('02-Outputs/maps/', pattern= '.tif', full.names = T)
-names <- list.files('02-Outputs/maps/', pattern= '.tif', full.names = F)
+files <- list.files(pattern= '.tif', full.names = T)
+names <- list.files( pattern= '.tif', full.names = F)
 names <- sub('.tif', '', names)
 
-Step1 <-data.frame(property_id =dt$property_id)
+
+# Switch depending on the naming system (i.e. files have e.g. Pav instead of p_0_30)
+#Step1 <-data.frame(property_id =dt$property_id)
+Step1 <-data.frame(property_id =dt$old_prop_ids) #old naming system
+
 Step1$Names <- 'Rename layer'
 Step1$Uncertainty <- 'Missing'
 
-for (i in unique(dt$property_id)){
-
-  t11 <- TRUE %in% grepl(paste0('sd_',i), files)
-  t12 <- TRUE %in% grepl(paste0('mean_',i), files)
+for (i in unique(Step1$property_id)){
+  
+  t11 <- TRUE %in% grepl(paste0('SD_GSNmap_',i), files)|grepl(paste0('sd_',i), files)
+  t12 <- TRUE %in% grepl(paste0(ISO,'_GSNmap_',i), files)|grepl(paste0('mean_',i), files)
+  
   t13 <- TRUE %in% grepl(ISO, files)
   
   Step1[Step1$property_id ==i, 'Names'] <- ifelse(t12[[1]] ==T & t13[[1]] ==T, 'Correctly named', 'Rename layer')
@@ -85,7 +94,7 @@ names(r) <- names
 # 3 - Step 3: Check the extent -------------------------------------------------
 # Check if the layers were masked with a cropland mask
 
-mask <- rast('01-Data/covs/mask.tif')
+mask <- rast('mask/mask.tif')
 mask <- project(mask, r[[1]])
 
 t <- r[[1]]
@@ -98,15 +107,16 @@ t3 <- sum(values(t, na.rm=T))-sum(values(mask, na.rm=T))
 
 
 # 4 - Step 4: Check the units, ranges, and outliers ----------------------------
-Step4 <- data.frame(property_id =dt$property_id)
+Step4 <- data.frame(property_id=Step1$property_id)
+#Step4 <- data.frame(property_id =dt$property_id)
 Step4$in_range <- 'Values not in range'
 
-for (i in unique(dt$property_id)){
-t41 <-min(values(r[[grepl(paste0('mean_',i), names(r))]],na.rm=T)) >=dt[dt$property_id == i, 'Min']
-t42 <-max(values(r[[grepl(paste0('mean_',i), names(r))]],na.rm=T)) <=dt[dt$property_id == i, 'Max']
-
-Step4[Step4$property_id ==i, 'in_range'] <- ifelse(t41[[1]] ==T & t42[[1]] ==T, 'Values in range', 'Values not in range')
-
+for (i in unique(Step4$property_id)){
+  t41 <-min(values(r[[grepl(paste0('mean_',i), names(r))|grepl(paste0(ISO,'_GSNmap_',i), names(r))]],na.rm=T)) >=dt[Step4$property_id == i, 'Min']
+  t42 <-max(values(r[[grepl(paste0('mean_',i), names(r))|grepl(paste0(ISO,'_GSNmap_',i), names(r))]],na.rm=T)) <=dt[Step4$property_id == i, 'Max']
+  
+  Step4[Step4$property_id ==i, 'in_range'] <- ifelse(t41[[1]] ==T & t42[[1]] ==T, 'Values in range', 'Values not in range')
+  
 }
 
 
@@ -120,4 +130,4 @@ report$extent <- ifelse(Step3, 'Croplands', 'Mask out layer')
 
 report
 
-write.csv(report, paste0('National Report/QA_QC_', ISO, '.csv'))
+write.csv(report, paste0('QA_QC_', ISO, '.csv'))
