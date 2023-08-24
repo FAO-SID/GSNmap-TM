6872414b184b80e26ab9e26a5d2b7d0d482b21ab
Marcos Angelini <angelini75@gmail.com>
2023-08-24 15:11:14 -0300

    bug in spline function

--- Digital-Soil-Mapping/03-Scripts/spline_functions.R
+++ Digital-Soil-Mapping/03-Scripts/spline_functions.R
@@ -3,6 +3,7 @@ library(purrr)
 apply_mpspline <- function(df, property, depth_range) {
   # Prepare input data for mpspline function
   input_data <- df[, c("ProfID", "top", "bottom", property)]
+  input_data <- na.omit(input_data)
   colnames(input_data) <- c("SID", "UD", "LD", "VAL")
   
   # Apply mpspline function
@@ -11,33 +12,35 @@ apply_mpspline <- function(df, property, depth_range) {
                               d = depth_range,
                               vlow = min(input_data$VAL), 
                               vhigh = max(input_data$VAL))
+  temp <- NULL
+  for (i in names(mpspline_result)){
+    t2 <- cbind(mpspline_result[[i]]$SID,mpspline_result[[i]]$est_dcm[1:(length(depth_range)-1)])
+    temp <- rbind(temp,t2)
+  }
+  temp <- as.data.frame(temp)
+  x <- str_split(row.names(temp), "\\.", simplify = TRUE)[,1]
+  temp$name <- x
+  names(temp) <- c("ProfID", "value", "name")
+  temp$value <- as.numeric(temp$value)
+  temp <- group_by(temp, ProfID)
   
-  # Extract predicted values for the depth range
-  xx <- sapply(mpspline_result, function(x) x$est_dcm)
-  xx <- purrr::map(xx, function(x) x[!is.na(names(x))])
-  predicted_values <- t(unique(as.data.frame(xx)))
-  predicted_values <- as.data.frame(predicted_values)[1:(length(depth_range)-1)]
-  predicted_values$ProfID <- sub(x = rownames(predicted_values), pattern = "X", replacement = "")
-  class(predicted_values$ProfID) <- class(df$ProfID)
-  
-  # Create a new dataframe with the interpolated values
-  result_df <- unique(data.frame(ProfID = df$ProfID))
-  result_df <- left_join(result_df, predicted_values)
   n <- length(depth_range)
   coln <- NULL
   for(layer in 2:n){
     coln <- append(coln, paste0(property,"_", depth_range[layer-1], "_", depth_range[layer]))
   }
-  names(result_df)[-1] <- coln
-  return(result_df)
+  temp <- pivot_wider(temp, id_cols = "ProfID", values_from = "value", names_from = name)
+  names(temp) <- c("ProfID", coln)
+  return(temp)
 }
 
 # Function to apply mpspline to all soil properties and depth ranges
 apply_mpspline_all <- function(df, properties, depth_range) {
   final_df <- data.frame(ProfID = df$ProfID)
   for (property in properties) {
+    if(any(!is.na(df[,property]))){
     x <- apply_mpspline(df, property, depth_range)
-    final_df <- left_join(final_df, x)
+    final_df <- left_join(final_df, x)}
   }
   final_df <- unique(final_df)
   return(final_df)
